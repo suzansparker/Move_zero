2ab535c253e645be257e41c46e6592516629b9f6
sudharsan <suzansparker30032003@gmail.com>
2023-07-24 22:56:45 +0530

    Move_zeroes

---
+++ Move_zeroes.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Move_zeroes {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the numbers");
+        int n=sc.nextInt();
+        int Ar[]=new int [n];
+        int sum=0;
+        for(int i=0;i<n;i++) {
+            Ar[i] = sc.nextInt();
+
+        if(Ar[i]!=0){
+
+             Ar[sum++] =Ar[i];}}
+             while(sum<n){
+                 Ar[sum++]=0;//System.out.println(Ar[i]+"");
+                 //
+                 }
+             for(int m=0;m<n;m++){System.out.println(Ar[m]+"");}
+             }
+
+
+
+}
